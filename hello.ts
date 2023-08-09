052666a945c8c9a1755312b7373e3a697ce290b9
laurenCho9 <lauren.choux@gmail.com>
2023-08-09 09:32:09 +0900

    docs: 타입스크립트 초기 세팅, 작성법

---
+++ hello.ts
@@ -0,0 +1,37 @@
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "owen";
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Owen",
+  age: 30,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+people = [
+  {
+    name: "Kevin",
+    age: 50,
+  },
+  {
+    name: "John",
+    age: 40,
+  },
+];
